9a5b8c07d40b1ed70c9be309ac03893b40239927
Matteo Ferrabone <matteo.ferrabone@gmail.com>
2020-11-10 00:31:32 +0100

    First Commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="timerdec",
+    version="0.0.1",
+    author="Matteo Ferrabone",
+    author_email="matteo.ferrabone@gmail.com",
+    license='MIT',
+    description="Simple decorators for measuring Python methods execution time",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/desmoteo/timerdec",
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'Topic :: Utilities ',
+    ],
+    python_requires='>=3.6',
+)
---
+++ test.py
@@ -0,0 +1,29 @@
+from timerdec.decorators import timerdec, timerdec_always
+import time
+import numpy as np
+
+@timerdec()
+def r(size1, size2):
+    return np.random.rand(size1, size2)
+
+class cl():
+    def __init__(self):
+        pass
+
+    @timerdec()
+    def f(self):
+        a = r(1000, 1000)
+        time.sleep(0.2)
+        b = r(1000, 1000)
+        time.sleep(0.2)
+        c = np.dot(a,b)
+
+    @timerdec_always(1000)
+    def dummy(self, a):
+        pass
+
+c = cl()
+
+c.f()
+c.dummy(a="dummy data")
+
